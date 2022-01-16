ebef2b1b59d4076d1811a93ccf6cc89192ce4d8b
Takobz <kobelakhutso.kk@gmail.com>
2022-01-16 16:26:58 +0200

    website refactor

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
+import Emoji from './Emoji';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'What Is This?',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <p>
+          I created this blogging site to help myself keep note of stuff I come across.
+          I have a bad record of having notes all over and it's hard to keep track sometimes. 
+          I came up with the title "Let's Pretend I Know This" since in the blogposts I will just be explaining stuff as 
+          I understand it and not really being a guru at any of the stuff.
+        </p>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    title: 'What do I do?',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <p>
+        I write software I am also trying to be active on open source environment.
+        I also enjoy reading about software thus writing some personal notes about
+        cool designs, practices, etc.
+        </p>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    title: 'Who am I?',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <p>
+        I am someone who stole{''} <a href="https://blog.johnnyreilly.com/about">John Reilly's</a> About page structure 
+        because why not <Emoji symbol='😀'></Emoji>.
+        I am software developer (relatively new to this). 
+        I went to the <a href='https://en.wikipedia.org/wiki/University_of_the_Witwatersrand'>University Of Witwatersrand</a> where I studies Computer Science.
+        </p>
       </>
     ),
   },
@@ -38,9 +47,9 @@ const FeatureList = [
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
+      {/* <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
-      </div>
+      </div> */}
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import Emoji from '../components/Emoji'
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -12,7 +13,14 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className="text--center">
+          <img
+            src='img/myinthefuture-jpeg.jpg'
+            alt="Khutso Profile Picture"
+            className={styles.profileImage}
+          />
+        </div>
+        <p className="hero__subtitle">{siteConfig.tagline} <Emoji symbol='📃'/></p>
         {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
